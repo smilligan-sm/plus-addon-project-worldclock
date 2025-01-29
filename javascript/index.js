f5b6db169624274a184878505ab43afb94d2cb60
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-01-29 12:09:27 +1100

    Added functionality to update date and time displayed when selection is made

--- javascript/index.js
+++ javascript/index.js
@@ -1,36 +1,61 @@
 function updateTime() {
   //hong kong
   let hongKongElement = document.querySelector("#hong-kong");
-  let hongKongDateElement = hongKongElement.querySelector(".date");
-  let hongKongTimeElement = hongKongElement.querySelector(".time");
-  let hongKongTime = moment().tz("Asia/Hong_Kong");
+  if (hongKongElement) {
+    let hongKongDateElement = hongKongElement.querySelector(".date");
+    let hongKongTimeElement = hongKongElement.querySelector(".time");
+    let hongKongTime = moment().tz("Asia/Hong_Kong");
 
-  hongKongDateElement.innerHTML = hongKongTime.format("MMMM Do YYYY");
-  hongKongTimeElement.innerHTML = hongKongTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    hongKongDateElement.innerHTML = hongKongTime.format("MMMM Do YYYY");
+    hongKongTimeElement.innerHTML = hongKongTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //los angeles
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
   //melbourne
   let melbourneElement = document.querySelector("#melbourne");
-  let melbourneDateElement = melbourneElement.querySelector(".date");
-  let melbourneTimeElement = melbourneElement.querySelector(".time");
-  let melbourneTime = moment().tz("Australia/Melbourne");
+  if (melbourneElement) {
+    let melbourneDateElement = melbourneElement.querySelector(".date");
+    let melbourneTimeElement = melbourneElement.querySelector(".time");
+    let melbourneTime = moment().tz("Australia/Melbourne");
 
-  melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do YYYY");
-  melbourneTimeElement.innerHTML = melbourneTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do YYYY");
+    melbourneTimeElement.innerHTML = melbourneTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+            <div class="city" id="hong-kong">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )}<small>${cityTime.format("A")}</small></div>
+        </div>
+  `;
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city-list");
+citiesSelectElement.addEventListener("change", updateCity);
